14ac25dbc5159191b6b3daa09c76999bd3319686
nobu0603 <nobking.63@gmail.com>
2024-05-02 18:36:04 +0900

    add: quasar ui components libraryを追加

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,24 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+  plugins: {} // import Quasar plugins and add here
+})
 
 app.mount('#app')
