d8362f855b02221200d740f1ef1de42f78b360f0
nobu0603 <nobking.63@gmail.com>
2024-05-04 01:36:18 +0900

    update: 画像の表示・非表示の切り替えを追加

--- frontend/src/views/ConvertImageView.vue
+++ frontend/src/views/ConvertImageView.vue
@@ -1,22 +1,51 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const model = ref(null)
+const imageUrl = ref('')
+const OnImage: boolean = false
+
+const showItem = () => {
+  const file = model.value
+  if (file) {
+    imageUrl.value = URL.createObjectURL(file)
+  }
+}
+
+const clearItem = () => {
+  imageUrl.value = ''
+}
 </script>
 
 <template>
   <div class="convert-image">
     <h2>This is a convert image page</h2>
-    <q-file filled bottom-slots v-model="model" label="Label" counter class="w-30">
+    <q-file
+      filled
+      bottom-slots
+      accept=".jpg, .jpeg, .JPEG, .png"
+      v-model="model"
+      label="Label"
+      counter
+      class="w-30"
+    >
       <template v-slot:prepend>
         <q-icon name="cloud_upload" @click.stop.prevent />
       </template>
       <template v-slot:append>
-        <q-icon name="close" @click.stop.prevent="model = null" class="cursor-pointer" />
+        <q-icon
+          name="close"
+          @click.stop.prevent="model = null"
+          @click="clearItem"
+          class="cursor-pointer"
+        />
       </template>
 
       <template v-slot:hint> Field hint </template>
     </q-file>
-    <q-btn color="primary" icon-right="sync" label="Convert Image" />
+    <q-btn color="primary" icon-right="image" label="Show Image" @click="showItem" />
+    <div class="w-30">
+      <img v-if="imageUrl" :src="imageUrl" alt="" class="w-100" />
+    </div>
   </div>
 </template>
 
@@ -30,7 +59,9 @@ const model = ref(null)
   justify-content: center;
   gap: 24px;
 }
-
+.w-100 {
+  width: 100%;
+}
 .w-30 {
   min-width: 300px;
   width: 30%;
